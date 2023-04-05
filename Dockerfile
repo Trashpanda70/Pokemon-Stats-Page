2bc7778f96e94d55ecde64eca1a23d7013933209
mdwelker <mattw8824@gmail.com>
2023-04-04 23:40:07 -0400

    fixed module exports for db.js

--- Dockerfile
+++ Dockerfile
@@ -3,18 +3,15 @@ FROM ubuntu:22.04
 EXPOSE 8080/tcp
 # Install necessary apt packages and nodejs
 RUN apt-get update
-RUN apt-get install -y curl sqlite3
+RUN apt-get install -y curl git sqlite3
 RUN curl -fsSL https://deb.nodesource.com/setup_19.x | bash - && apt-get install -y nodejs
 # Set up directory for app
-RUN mkdir app
-COPY package.json /app/
-COPY *.sql /app/
-COPY *.js /app/
-COPY .eslintrc.json /app/
+RUN git clone https://github.com/Trashpanda70/Pokemon-Stats-Page.git app
 WORKDIR /app
 # create sqlite3 (test) database
-RUN mkdir db
-RUN sqlite3 db/todos.db < test-init.sql
+RUN mkdir db-files
+RUN sqlite3 db-files/todos.db < test-init.sql
 # install dependencies and start program
 RUN npm install
-CMD npm start
+CMD /bin/bash
+
--- database/db.js
+++ database/db.js
@@ -1,27 +1,27 @@
 const sqlite3 = require('sqlite3').verbose();
 const ServerError = require('./utils/ServerError');
 
-let defaultPath = './db/Database.db';
+let defaultPath = './db-files/todos.db';
 
 //open database connection
-function connect(path = defaultPath) {
+exports.connect = function connect(path = defaultPath) {
   return new sqlite3.Database(path, (err) => {
     if (err) {
       return console.error(`An error occured connecting to database. Please try again or report this. Message below:\n${err.message}`);
     }
     console.log('Connected to database!');
   });
-}
+};
 
 //close database connection
-function close(db) {
+exports.close = function close(db) {
   db.close((err) => {
     if (err) {
       return console.error(`An error occured closing the database. Please report this. Message below:\n${err.message}`);
     }
     console.log('Connection to database closed');
   });
-}
+};
 
 /*
   db - database object
@@ -29,12 +29,12 @@ function close(db) {
   callback - the callback function to pass to db.get
   args - array of arguments to give to SQL command (replace ? in command)
 */
-function execGetCommand(db, command, callback, args = []) {
+exports.execGetCommand = function execGetCommand(db, command, callback, args = []) {
   //https://stackoverflow.com/questions/13286233
   if (command.match(/\?/g) != args.length)
     throw new ServerError("Not enough arguments given for get query", 400);
   db.get(command, args, callback);
-}
+};
 
 /*
   db is database object
@@ -42,12 +42,12 @@ function execGetCommand(db, command, callback, args = []) {
   callback is the callback function to pass to db.get
   args is array of arguments to give to SQL command (replace ? in command)
 */
-function execEachCommand(db, command, args = [], callback = () => { }) {
+exports.execEachCommand = function execEachCommand(db, command, args = [], callback = () => { }) {
   throw new ServerError("'each' command not implemented yet", 501);
   // if (command.match(/\?/g) != args.length)
   //   throw new ServerError("Not enough arguments given for get query", 400);
   // db.each(command, args, callback);
-}
+};
 
 /*
   db is database object
@@ -55,7 +55,7 @@ function execEachCommand(db, command, args = [], callback = () => { }) {
   callback is the callback function to pass to db.get
   args is array of arguments to give to SQL command (replace ? in command)
 */
-function execAllCommand(db, command, args = []) {
+exports.execAllCommand = function execAllCommand(db, command, args = []) {
   if (command.match(/\?/g) != args.length)
     throw new ServerError("Not enough arguments given for get query", 400);
   return new Promise((resolve, reject) => {
@@ -69,16 +69,16 @@ function execAllCommand(db, command, args = []) {
       resolve(rows);
     });
   });
-}
+};
 
 // - all() method is used when you need to fetch and process all rows from the result set at once
 // - each() method is used when you need to fetch and process each row from the result set one by one
 // - get() method is used when you need to fetch and process only the first row from the result set
 
 // Run a command without need for a return or success / fail
-function runCommand(db, command) {
+exports.runCommand = function runCommand(db, command) {
   db.run(command);
-}
+};
 
 // /* 
 //   Insert data into the specified table
@@ -92,15 +92,3 @@ function runCommand(db, command) {
 //   let cmdString = `INSERT INTO ${table}()`;
 //   db.run();
 // }
-
-
-
-
-module.exports = {
-  connect,
-  close,
-  execGetCommand,
-  execEachCommand,
-  execAllCommand,
-  runCommand
-};
